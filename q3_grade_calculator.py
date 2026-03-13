864e10568cfd84afecb3c1c13becf9dc57794cb2
Jackline Kairuthi <jackiekairuthi@gmail.com>
2026-03-13 23:57:05 +0000

    Add grade calculator based on student scores

---
+++ q3_grade_calculator.py
@@ -0,0 +1,10 @@
+student_marks = int(input("Student score:"))
+
+if student_marks >= 90:
+  print("Grade A")
+elif student_marks >=75:
+  print("Grade B")
+elif student_marks >=50:
+  print("Grade C")
+else:
+  print("Fail")
