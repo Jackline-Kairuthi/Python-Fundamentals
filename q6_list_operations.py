2062a9a5e38f44fda0f80f9d55798128c87996fd
Jackline Kairuthi <jackiekairuthi@gmail.com>
2026-03-14 00:00:29 +0000

    Add list operations for user input numbers

---
+++ q6_list_operations.py
@@ -0,0 +1,10 @@
+N1 = int(input("Enter a number: "))
+N2 = int(input("Enter a number: "))
+N3 = int(input("Enter a number: "))
+N4 = int(input("Enter a number: "))
+N5 = int(input("Enter a number: "))
+
+List = [N1, N2, N3, N4, N5]
+print(List)
+print("The maximum number is: ", max(List))
+print("The minimum number is: ", min(List))
