34df9e6cba4719c400f7d29c297aef4dfdb79db4
Jackline Kairuthi <jackiekairuthi@gmail.com>
2026-03-14 00:01:33 +0000

    Create script to collect unique numbers in a set
    This script takes five numbers as input and stores them in a set to ensure uniqueness.

---
+++ q8_unique_elements.py
@@ -0,0 +1,9 @@
+N1 = int(input("first number: "))
+N2 = int(input("second number: "))
+N3 = int(input("third number: "))
+N4 = int(input("fourth number: "))
+N5 = int(input("fifth number: "))
+
+Set = {N1, N2, N3, N4, N5}
+print(Set)  
+
